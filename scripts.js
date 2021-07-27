95fab377ed5e1e89d2124f5131e3c88de2df6876
Bruno <mguimaraesbruno@gmail.com>
2021-07-27 11:43:37 -0300

    preparando para o review

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function selecionarprato (elemento){
-    const botaomarcado = document.querySelector(".prato");
-    if (botaomarcado !== null){
-    }
-     
-    elemento.classList.toggle("verde");
-
-}
